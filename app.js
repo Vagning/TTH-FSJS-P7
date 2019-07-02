d69d453937572192b11a336a4c31af2267749d86
Thomas Vagning <thv@kamstrup.com>
2019-07-02 14:47:46 +0200

    Update pug templates

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //Require neccesary dependencies
 const express = require('express');
 const path = require('path');
-const data = require('./data.json');
+const { projects } = require('./data.json');
 //Declare variables
 const app = express();
 //Set pug as the view engine for this project
 app.set('view engine', 'pug');
+//Set the folder plublic to be used for providing static content
+app.use('/static', express.static('public'));
 //Determin which port the express should listen on
 app.listen(3000, () => {
 	console.log('Application is listening on localhost:3000')
@@ -15,5 +17,4 @@ const mainRoutes = require('./routes');
 const projectRoutes = require('./routes/projects');
 app.use(mainRoutes);
 app.use('/projects', projectRoutes);
-//Set the folder plublic to be used for providing static content
-app.use(express.static('plublic'));
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 //Require neccesary dependencies
 const express = require('express');
 const router = express.Router();
+const { projects } = require('../data.json');
 //"Index" route to render "Home" page
 router.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', {projects: projects});
 });
 //"About" route to render "About" page
 router.get('/about', (req, res) => {
