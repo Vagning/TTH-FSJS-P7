63c4241a857e1b0bb19b4cd8df3adc7b0b189ef4
Thomas Vagning <thv@kamstrup.com>
2019-07-04 11:11:14 +0200

    Add validation of requested project

--- routes/projects.js
+++ routes/projects.js
@@ -3,12 +3,21 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-router.get('/:id', (req, res) => {
-	//Set id as variable
+router.get('/:id', (req, res, next) => {
+	//Set id as variable, set project as variable
 	const { id } = req.params;
 	const project = projects[id];
-	//Render project pug template with input values for correct project
-	res.render('project', {project});
+
+	//Check if project exsists
+	if (project) {
+		//Render project pug template with input values for correct project
+		res.render('project', {project});
+	} else {
+		//If request project doesn't exsist throw error
+		const error = new Error('Project not found');
+		next(error);
+	}
+
 });
 
 module.exports = router;
